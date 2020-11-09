f8077cda0315e4eac2542cd85ad73cc4596d941f
committer <git@necz.net>
2020-11-09 20:57:45 +0700

    Only import required modules

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,9 @@
 
 import config from './config.js';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
 
 firebase.initializeApp(config.firebase);
 
